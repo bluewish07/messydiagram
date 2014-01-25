8a721f4180766606c1a4e51d9a3de7202f052c9f
Henry Shih <hunshih@umich.edu>
2014-01-25 12:25:09 -0500

    load data from daneliza to user schemas

---
+++ loadData.sql
@@ -0,0 +1,12 @@
+drop table user_table
+/
+create table user_schema as (select * from daneliza.public_user_information)
+/
+create table friends_schema as (select * from daneliza.public_are_friends)
+/
+create table photo_schema as (select * from daneliza.public_photo_information)
+/
+create table tag_schema as (select * from daneliza.public_tag_information)
+/
+create table event_schema as (select * from daneliza.public_event_information)
+/
